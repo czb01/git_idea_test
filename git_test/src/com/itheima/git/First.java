2e313ddc85a472b4f762102fbdaf20cafe562dd7
曹智博 <czb2013nj@163.com>
2018-12-29 16:11:53 +0800

    把本地工程添加到git测试1

--- git_test/src/com/itheima/git/First.java
+++ git_test/src/com/itheima/git/First.java
@@ -4,4 +4,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+    public void test(){
+        int i=0;
+        System.out.println(i);
+    }
 }
