711e141b874eae88adf4e45f8c7567b476cc5720
czb01 <46237632+czb01@users.noreply.github.com>
2018-12-29 16:23:02 +0800

    Update First.java

--- git_test/src/com/itheima/git/First.java
+++ git_test/src/com/itheima/git/First.java
@@ -3,7 +3,7 @@ package com.itheima.git;
 public class First {
     public static void main(String[] args) {
         System.out.println("hello git");
-        //hello
+        //hello asdasdad
     }
     public void test(){
         int i=0;
