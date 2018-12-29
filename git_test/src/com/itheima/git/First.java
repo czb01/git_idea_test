2e128ab130fa5c40fa4a5a17a46d63b05e152b2e
曹智博 <czb2013nj@163.com>
2018-12-29 16:20:30 +0800

    本次修改

--- git_test/src/com/itheima/git/First.java
+++ git_test/src/com/itheima/git/First.java
@@ -3,6 +3,7 @@ package com.itheima.git;
 public class First {
     public static void main(String[] args) {
         System.out.println("hello git");
+        //hello
     }
     public void test(){
         int i=0;
